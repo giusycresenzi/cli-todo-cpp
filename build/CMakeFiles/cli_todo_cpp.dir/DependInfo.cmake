105600826a92d875d61bd809313358ed5b5221da
Giuseppe Crescenzi <giuseppecrescenzi17p@gmail.com>
2025-04-12 17:14:48 +0200

    minor adjustments, getters and setters, all methods parameters and return values checked

---
+++ build/CMakeFiles/cli_todo_cpp.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/giuseppecrescenzi/cli-todo-cpp/src/CommandHandler.cpp" "CMakeFiles/cli_todo_cpp.dir/src/CommandHandler.cpp.o" "gcc" "CMakeFiles/cli_todo_cpp.dir/src/CommandHandler.cpp.o.d"
+  "/Users/giuseppecrescenzi/cli-todo-cpp/src/main.cpp" "CMakeFiles/cli_todo_cpp.dir/src/main.cpp.o" "gcc" "CMakeFiles/cli_todo_cpp.dir/src/main.cpp.o.d"
+  "/Users/giuseppecrescenzi/cli-todo-cpp/src/todo.cpp" "CMakeFiles/cli_todo_cpp.dir/src/todo.cpp.o" "gcc" "CMakeFiles/cli_todo_cpp.dir/src/todo.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/cli_todo_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cli_todo_cpp.dir/src/CommandHandler.cpp.o"
+  "CMakeFiles/cli_todo_cpp.dir/src/CommandHandler.cpp.o.d"
+  "CMakeFiles/cli_todo_cpp.dir/src/main.cpp.o"
+  "CMakeFiles/cli_todo_cpp.dir/src/main.cpp.o.d"
+  "CMakeFiles/cli_todo_cpp.dir/src/todo.cpp.o"
+  "CMakeFiles/cli_todo_cpp.dir/src/todo.cpp.o.d"
+  "cli_todo_cpp"
+  "cli_todo_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cli_todo_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
